1801b6823719c2cf436b52394a4b519aab42fb28
Aislin :) <10750181+aislinp@users.noreply.github.com>
2023-02-01 20:35:55 +0000

    fdsf

--- src/drugs.js
+++ src/drugs.js
@@ -1,13 +1,23 @@
-const drugs = {
+/*
+{'name': 'Name': 'fullName': 'scientific name', 'effects': ['side', 'effects'], 'signs': ['signs', 'of', 'overdose'], 'appearance': ['what', 'it', 'looks', 'like']},
+{'name': 'Name': 'fullName': 'scientific name', 'effects': ['side', 'effects'], 'signs': ['signs', 'of', 'overdose'], 'appearance': ['what', 'it', 'looks', 'like'], 'category': 'category'}
+*/
+
+let drugs = {
   'drugs': [
-	  {'name': 'LSD', 'fullName': 'lysergic acid diethylamide', 'effects': ['changes in perception', 'changes in mood', 'changes in thought', 'hallucinations', 'space distortion', 'time distortion'], 'signs': ['euphoria', 'pupil dilation', 'visual hallucinations', 'auditory hallucinations', 'confusion', 'trouble concentrating', 'headaches', 'nausea', 'fast heartbeat', 'irregular heartbeat', 'increased body temperature', 'fast breathing', 'vomiting', 'facial flushes', 'sweating', 'chills'], 'appearance': ['tabs', 'liquid', 'gelatin sheets', 'capsules'], 'category': 'psychedelic'},
-	  {'name': 'Magic Mushrooms', 'fullName': 'psilocybin', 'effects': ['euphoria', 'altered visuals', 'time distortion', 'mild nausea', 'sweating', 'auditory hallucinations', 'visual hallucinations', 'space distortion'], 'signs': ['anxiety', 'paranoia', 'delusions'], 'appearance': ['mushrooms with a cap and a long stem', 'gray', 'brown', 'white', 'tan', 'white powder', 'dried mushrooms'], 'category': 'psychedelic'},
+    {'name': 'LSD', 'fullName': 'lysergic acid diethylamide', 'effects': ['changes in perception', 'changes in mood', 'changes in thought', 'hallucinations', 'space distortion', 'time distortion'], 'signs': ['euphoria', 'pupil dilation', 'visual hallucinations', 'auditory hallucinations', 'confusion', 'trouble concentrating', 'headaches', 'nausea', 'fast heartbeat', 'irregular heartbeat', 'increased body temperature', 'fast breathing', 'vomiting', 'facial flushes', 'sweating', 'chills'], 'appearance': ['tabs', 'liquid', 'gelatin sheets', 'capsules'], 'category': 'psychedelic'},
+    {'name': 'Magic Mushrooms', 'fullName': 'psilocybin', 'effects': ['euphoria', 'altered visuals', 'time distortion', 'mild nausea', 'sweating', 'auditory hallucinations', 'visual hallucinations', 'space distortion'], 'signs': ['anxiety', 'paranoia', 'delusions'], 'appearance': ['mushrooms with a cap and a long stem', 'gray', 'brown', 'white', 'tan', 'white powder', 'dried mushrooms'], 'category': 'psychedelic'},
     {'name': 'DMT', 'fullName': 'N-Dimethyltryptamine', 'effects': ['rush', 'euphoria', 'changes in mood', 'increased sensitivity', 'fast heartbeat', 'increased blood pressure', 'dilated pupils', 'visual hallucinations', 'dissociation'], 'signs': ['anxiety', 'delusions', 'psychosis', 'hallucinations', 'paranoia'], 'appearance': ['white powder', 'crystalline']},
     {'name': 'Mescaline', 'fullName': '5-trimethoxyphenethylamine', 'effects': ['euphoria', 'increased energy', 'space distortion', 'time distortion', 'trouble concentrating', 'trouble thinking', 'increased blood pressure', 'fast heartbeat', 'intense nausea', 'vomiting'], 'signs': ['overdoses are rare.'], 'appearance': ['powder', 'tablet', 'capsule', 'liquid']},
     {'name': 'NBOMes', 'fullName': '25-NB', 'effects': ['visual hallucinations', 'auditory hallucinations', 'euphoria', 'sweating', 'confusion', 'memory issues', 'increased blood pressure', 'fast heartbeat', 'paranoia'], 'signs': ['intense confusion', 'agitation', 'seizure', 'kidney failure', 'nausea', 'vomiting', 'overheating', 'coma'], 'appearance': ['blotters', 'liquid', 'powder', 'pill']},
     {'name': 'Salvia', 'fullName': 'Saliva divinorum', 'effects': ['anxiety', 'amnesia', 'hallucinations', 'sedation', 'low energy', 'pain relief', 'confusion', 'delusion', 'visual distortions', 'laughter', 'time distortion'], 'signs': ['overdoses are rare.'], 'appearance': ['fresh leaves', 'dried leaves']}
   ]
-}
+};
+
+document.getElementById('emergencyButtonDiv').addEventListener('click', function f(e) {
+  window.location.assign('/emergency.html');
+  document.getElementById('emergencyButtonDiv').removeEventListener('click', f);
+});
 
 for (let i = 0; i < drugs.drugs.length; i++) {
   let drug = drugs.drugs[i];
@@ -38,10 +48,10 @@ for (let i = 0; i < drugs.drugs.length; i++) {
   title.classList.add('textButton', 'divTitle');
   title.addEventListener('click', () => {
     buildInfoDiv(drug);
-  })
+  });
 
   caption.innerText = 'Appearance, dangers, and symptoms';
-  caption.classList.add('divCaption')
+  caption.classList.add('divCaption');
 
   photo.src = `/img/${drug.name.replace(/\s/g, '')}.png`;
   photo.classList.add('articleIMG');
@@ -76,11 +86,11 @@ function buildInfoDiv(drug) {
   name.classList.add('divTitle');
   name.innerText = drug.name;
 
-  caption.classList.add('divCaption')
+  caption.classList.add('divCaption');
   caption.innerHTML = `scientific name: <i>${drug.fullName}</i>`;
 
-  wikiLink.innerText = `Wikipedia link to ${drug.name}.`
-  wikiLink.href = `https://en.wikipedia.org/wiki/${drug.name}`
+  wikiLink.innerText = `Wikipedia link to ${drug.name}.`;
+  wikiLink.href = `https://en.wikipedia.org/wiki/${drug.name}`;
   wikiLink.target = '_blank';
   wikiLink.rel = 'noopener noreferrer';
 
@@ -97,7 +107,7 @@ function buildInfoDiv(drug) {
   for (let i = 0; i < drug.signs.length; i++) {
     let sign = document.createElement('LI');
     sign.innerText = drug.signs[i];
-    signs.appendChild(sign)
+    signs.appendChild(sign);
   }
 
   appearanceLabel.innerText = `The possible appearances of ${drug.name} are as follows:`;
@@ -105,7 +115,7 @@ function buildInfoDiv(drug) {
   for (let i = 0; i < drug.appearance.length; i++) {
     let appearance = document.createElement('LI');
     appearance.innerText = drug.appearance[i];
-    appearances.appendChild(appearance)
+    appearances.appendChild(appearance);
   }
 
   let infoNodes = [closeButton, name, caption, wikiLink, effectsLabel, effects, signsLabel, signs, appearanceLabel, appearances];
@@ -117,37 +127,37 @@ function buildInfoDiv(drug) {
   
   closeButton.addEventListener('click', () => {
     for (i = 0; i < document.getElementById('bodyDiv').childNodes.length; i++) {
-		  if (document.getElementById('bodyDiv').childNodes[i].id == 'infoDiv') {
-			  document.getElementById('bodyDiv').removeChild(document.getElementById('bodyDiv').childNodes[i]);
-		  }
-	  }
+      if (document.getElementById('bodyDiv').childNodes[i].id == 'infoDiv') {
+        document.getElementById('bodyDiv').removeChild(document.getElementById('bodyDiv').childNodes[i]);
+      }
+    }
   });
   window.addEventListener('keyup', function f(e) {
     if (e.key == 'Escape') {
       for (i = 0; i < document.getElementById('bodyDiv').childNodes.length; i++) {
-		    if (document.getElementById('bodyDiv').childNodes[i].id == 'infoDiv') {
-			    document.getElementById('bodyDiv').removeChild(document.getElementById('bodyDiv').childNodes[i]);
-		    }
-	    }
-			window.removeEventListener('keyup', f);
+        if (document.getElementById('bodyDiv').childNodes[i].id == 'infoDiv') {
+          document.getElementById('bodyDiv').removeChild(document.getElementById('bodyDiv').childNodes[i]);
+        }
+      }
+      window.removeEventListener('keyup', f);
     }
   });
 
   document.getElementById('bodyDiv').appendChild(container);
 }
 
-var searchDrugs = document.getElementById('searchBar');
+let searchDrugs = document.getElementById('searchBar');
 searchDrugs.addEventListener('keyup', search);
 
 function search() {
-	let regEx = /\s|\.|\'/g
-	var filter = searchDrugs.value.replace(regEx, '-').toLowerCase();
-	drugNodes = document.getElementsByClassName('article');
-	for (i = 0; i < drugNodes.length; i++) {
+  let regEx = /\s|\.|\'/g;
+  let filter = searchDrugs.value.replace(regEx, '-').toLowerCase();
+  drugNodes = document.getElementsByClassName('article');
+  for (i = 0; i < drugNodes.length; i++) {
     let showDiv = false;
     for (let j = 0; j < drugNodes[i].classList.length; j++) {
       if (drugNodes[i].classList[j].toLowerCase().indexOf(filter) > -1) {
-			  showDiv = true;
+        showDiv = true;
         break;
       }
     }
@@ -156,5 +166,5 @@ function search() {
     } else{
       drugNodes[i].style.display = 'none';
     }
-	}
+  }
 }
--- src/emergency.js
+++ src/emergency.js
@@ -1,18 +1,46 @@
-const hotlines = {
-  'list': [
-    {'name': '911 (National Emergency Hotline)', 'number': 911, 'national': true, '247': true},
-    {'name': 'National Poison Control Center', 'number': 18002221222, 'national': true, '247': true},
-    {'name': '24/7 Drug Abuse & Addiction Helpline', 'number': 18442890879, 'national': true, '247': true},
-    {'name': 'Boys Town National Hotline', 'number': 18004483000, 'national': true, '247': true},
-    {'name': 'SAMHSA National Hotline', 'number': 18006624357, 'national': true, '247': true},
-    {'name': 'Cocaine Addiction Treatment', 'number': 18005599503, 'national': true, '247': true},
-    {'name': 'Center for Substance Abuse', 'number': 18777264727, 'national': true, '247': true},
-    {'name': 'Alabama Mental Health & Recovery', 'number': 18443071760, 'national': false, '247': true, 'location': 'Alabama'},
-    {'name': 'Alaska\'s 2-11 Drug Hotline', 'number': 211, 'national': false, '247': false, 'location': 'Alaska', 'hours': 'M-F 8:30-5', 'location': 'Alaska'},
-    {'name': 'Alaska\'s Mental Health Careline', 'number': 8772664357, 'national': false, '247': true, 'location': 'Alaska'},
-    {'name': 'Arizona\'s 2-11 Drug Hotline', 'number': 211, 'national': false, '247': true, 'location': 'Arizona'}
-  ]
+function filterLocation(filter) {
+  let hotlineNodes = document.getElementsByClassName('hotline');
+  for (let i = 0; i < hotlineNodes.length; i++) {
+    let showDiv = false;
+    for (let j = 0; j < hotlineNodes[i].classList.length; j++) {
+      if (hotlineNodes[i].classList[j].toLowerCase().indexOf(filter) > -1) {
+        showDiv = true;
+        break
+      } else if(hotlineNodes[i].classList[j].toLowerCase().indexOf('national') > -1) {
+        showDiv = true;
+        break
+      }
+    }
+    if (showDiv) {
+      hotlineNodes[i].style.display = 'inline-block';
+    } else{
+      hotlineNodes[i].style.display = 'none';
+    }
+  }
 }
+
+var reverseGeocoder = new BDCReverseGeocode();
+reverseGeocoder.getClientLocation(function(result) {
+  if (!result.countryCode == 'US') {
+    return
+  }
+  filterLocation(result.principalSubdivision);
+});
+
+let hotlines = {
+  'list': [
+    {'name': '911 (National Emergency Hotline)', 'number': 911, 'national': true, '247': true, 'features': ['Ongoing overdoses.', 'Crimes.']},
+    {'name': 'National Poison Control Center', 'number': 18002221222, 'national': true, '247': true, 'features': ['Assessing the severity of overdoses.', 'Giving steps on how to help those in overdose situations.']},
+    {'name': '24/7 Drug Abuse & Addiction Helpline', 'number': 18442890879, 'national': true, '247': true, 'features': ['Information on drug addiction.', 'Information on the treatment of drug addiction.']},
+    {'name': 'Boys Town National Hotline', 'number': 18004483000, 'national': true, '247': true, 'features': ['Multi-issue crises.']},
+    {'name': 'SAMHSA National Hotline', 'number': 18006624357, 'national': true, '247': true, 'features': ['Referrals and information on substance use disorders and mental health problems.']},
+    {'name': 'Center for Substance Abuse', 'number': 18777264727, 'national': true, '247': true, 'features': ['Information about mental and substance use disorders']},
+    {'name': 'Alabama Mental Health & Recovery', 'number': 18443071760, 'national': false, '247': true, 'location': 'Alabama', 'features': ['Referrals to Alabaman services to treat drug addiction.']},
+    {'name': 'Alaska\'s 2-11 Drug Hotline', 'number': 211, 'national': false, '247': false, 'location': 'Alaska', 'hours': 'M-F 8:30-5', 'location': 'Alaska', 'features': ['General helpline for Alaska.']},
+    {'name': 'Alaska\'s Mental Health Careline', 'number': 8772664357, 'national': false, '247': true, 'location': 'Alaska', 'features': ['Suicide prevention and crisis intervention.']},
+    {'name': 'Arizona\'s 2-11 Drug Hotline', 'number': 211, 'national': false, '247': true, 'location': 'Arizona', 'features': ['General helpline for Alaska.']}
+  ]
+};
 
 for (let i = 0; i < hotlines.list.length; i++) {
   let currentHotline = hotlines.list[i];
@@ -20,19 +48,23 @@ for (let i = 0; i < hotlines.list.length; i++) {
   let container = document.createElement('DIV');
   let title = document.createElement('P');
   let number = document.createElement('A');
-  let features = document.createElement('UL');
+  let infoList = document.createElement('UL');
   let alwaysOpen = document.createElement('LI');
   let location = document.createElement('LI');
+  let featuresLabel = document.createElement('P');
+  let featuresList = document.createElement('UL');
   
   container.classList.add(currentHotline.name.replace(/\s/g, '-'));
   container.classList.add('article', 'hotline');
 
   if (currentHotline['247']) {
     container.classList.add('24/7');
-  }
+  };
   if (currentHotline['national']) {
     container.classList.add('national');
-  }
+  } else {
+    container.classList.add(currentHotline['location']);
+  };
 
   title.innerText = currentHotline.name;
   title.classList.add('divTitle');
@@ -44,12 +76,20 @@ for (let i = 0; i < hotlines.list.length; i++) {
   alwaysOpen.innerText = currentHotline['247'] ? 'Always Open' : `Hours: ${currentHotline['hours']}`;
   location.innerText = currentHotline['national'] ? 'Location: National' : `Location: ${currentHotline['location']}`;
 
-  features.appendChild(alwaysOpen);
-  features.appendChild(location);
+  infoList.appendChild(alwaysOpen);
+  infoList.appendChild(location);
+
+  featuresLabel.innerText = 'What they can help you with:';
+  for (let j = 0; j < currentHotline['features'].length; j++) {
+    let feature = document.createElement('LI');
+    feature.innerText = currentHotline['features'][j];
+    featuresList.appendChild(feature);
+  }
   
   container.appendChild(title);
   container.appendChild(number);
-  container.appendChild(features);
+  container.appendChild(featuresLabel);
+  container.appendChild(featuresList);
 
   document.getElementById('bodyDiv').appendChild(container);
 }
@@ -58,14 +98,14 @@ var searchHotlines = document.getElementById('searchBar');
 searchHotlines.addEventListener('keyup', search);
 
 function search() {
-	let regEx = /\s|\.|\'/g
-	var filter = searchHotlines.value.replace(regEx, '-').toLowerCase();
-	hotlineNodes = document.getElementsByClassName('hotline');
-	for (i = 0; i < hotlineNodes.length; i++) {
+  let regEx = /\s|\.|\'/g;
+  var filter = searchHotlines.value.replace(regEx, '-').toLowerCase();
+  let hotlineNodes = document.getElementsByClassName('hotline');
+  for (let i = 0; i < hotlineNodes.length; i++) {
     let showDiv = false;
     for (let j = 0; j < hotlineNodes[i].classList.length; j++) {
       if (hotlineNodes[i].classList[j].toLowerCase().indexOf(filter) > -1) {
-			  showDiv = true;
+        showDiv = true;
         break;
       }
     }
@@ -74,5 +114,5 @@ function search() {
     } else{
       hotlineNodes[i].style.display = 'none';
     }
-	}
+  }
 }
